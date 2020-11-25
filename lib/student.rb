750b5921ce760f1f20e0ba2b5809db012095a634
shabaz2020 <shabaz8010@gmail.com>
2020-11-25 11:54:31 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,12 @@
-class Student 
+class Student
 
-    def hello
+    def hello 
         puts "Hey there! I'm so excited to learn stuff."
     end
 
     def raise_hand
         puts "Pick me!"
     end
+    ''
 
 end
